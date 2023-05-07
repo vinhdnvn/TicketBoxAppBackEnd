07ab8eb682752ba5ee5095687f2694d48eafd88b
vinhdnvn <vinh120203@gmail.com>
2023-05-07 17:32:54 +0700

    done uploadFile routing !

---
+++ config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+export default {
+	firebase: {
+		apiKey: "AIzaSyCYSy6HA_wcK6ARpC7L_-r15aj9ZpGryNQ",
+		authDomain: "ticketbox-dd2b9.firebaseapp.com",
+		projectId: "ticketbox-dd2b9",
+		storageBucket: "ticketbox-dd2b9.appspot.com",
+		messagingSenderId: "244272189056",
+		appId: "1:244272189056:web:0ce20051b11617e349a065",
+		measurementId: "G-CLYXRY7DHP",
+	},
+};
+
+// const app = initializeApp(firebaseConfig);
+// const storage = getStorage(app);
+
+// export { storage };
